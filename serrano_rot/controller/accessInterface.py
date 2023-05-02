5370c7358153a9699deee921d9b8302c048b5668
Fotis Kouzinos <fotis.kouzinos@gmail.com>
2023-05-02 16:54:21 +0300

    unittest

--- serrano_rot/controller/accessInterface.py
+++ serrano_rot/controller/accessInterface.py
@@ -260,6 +260,6 @@ class AccessInterface(QThread):
     def run(self):
         logger.info("AccessInterface is running ...")
 
-        context = ('/home/jenkins/agent/workspace/serrano-rot-pipeline/certs/cert.pem',
-                   '/home/jenkins/agent/workspace/serrano-rot-pipeline/certs/key.pem')
+        context = ('/home/jenkins/agent/workspace/serrano-rot-pipeline/serrano_rot/certs/cert.pem',
+                   '/home/jenkins/agent/workspace/serrano-rot-pipeline/serrano_rot/certs/key.pem')
         self.rest_app.run(host=self.address, port=self.port, debug=False, ssl_context=context)
